a5bac77ba6bbab1b2720b7b9d1197f04737e6f01
onorbumbum <bumbum@onorbumbum.com>
2026-02-19 13:13:47 -0800

    Remove custom user agent from BrowserTab

--- aslan-browser/BrowserTab.swift
+++ aslan-browser/BrowserTab.swift
@@ -47,7 +47,6 @@ class BrowserTab: NSObject, WKNavigationDelegate, NSWindowDelegate {
         let frame = NSRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height))
 
         let wv = WKWebView(frame: frame, configuration: config)
-        wv.customUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
         self.webView = wv
 
         let win = NSWindow(
